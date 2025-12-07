f2353573d33932252e1369cce8c4560fd8569fd0
adarshanand67 <adarshan20302@gmail.com>
2025-12-07 12:48:31 +0530

    style: default to dark mode, ignore system preference

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${assistant.variable} ${jetbrains.variable} antialiased`}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+        <ThemeProvider attribute="class" defaultTheme="dark" disableTransitionOnChange>
           <Navbar />
           <MatrixRain />
           <CommandMenu />
